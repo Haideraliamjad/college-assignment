01fb1bd0855e873663775b6ab059460e6b8b11ea
Haider <haideraliamjad4391@gmail.com>
2023-09-05 21:18:57 +0500

    add : cpp assignment

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This repository contain the assignments of the subject programming fundamentals
 ### Technology Stack
 
 Flowgorithm
+Cpp
 
 
 
@@ -14,5 +15,6 @@ Flowgorithm
 
 * How to track project versions
 * How to create flowcharts using flowgorithm
+* Basics of cpp programming
 
 
---
+++ cpp/sizeof.cpp
@@ -0,0 +1,17 @@
+/*
+@title : Find the size of datatype in cpp
+@written By : Haider Ali Amjad
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    // we can find the size of any datatype in cpp using sizeof() operator
+    cout << "the size of int is " << sizeof(int) << " bytes" << endl;
+    cout << "the size of int is " << sizeof(bool) << " bytes" << endl;
+    cout << "the size of int is " << sizeof(float) << " bytes" << endl;
+    cout << "the size of int is " << sizeof(double) << " bytes" << endl;
+    cout << "the size of int is " << sizeof(char) << " bytes" << endl;
+
+    return 0;
+}
